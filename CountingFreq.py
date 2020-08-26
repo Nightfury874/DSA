9de55a3a46c43a7c107ed577ae11137c797d1d20
Nihar Jyoti Basisth <62636996+Nightfury874@users.noreply.github.com>
2020-08-26 20:30:50 +0530

    Update CountingFreq.py

--- CountingFreq.py
+++ CountingFreq.py
@@ -32,3 +32,30 @@ if __name__ == '__main__':
       
  # Time Complexity : O(n2)
  # Auxiliary Space : O(n)
+      
+# Python3 program to count frequencies  
+# of array items 
+def countFreq(arr, n): 
+  
+    mp = dict() 
+  
+    # Traverse through array elements  
+    # and count frequencies 
+    for i in range(n): 
+        if arr[i] in mp.keys(): 
+            mp[arr[i]] += 1
+        else: 
+            mp[arr[i]] = 1
+              
+    # Traverse through map and print  
+    # frequencies 
+    for x in mp: 
+        print(x, " ", mp[x]) 
+  
+# Driver code 
+arr = [10, 20, 20, 10, 10, 20, 5, 20 ] 
+n = len(arr) 
+countFreq(arr, n) 
+  
+      
+  #Time Complexity O(n)
